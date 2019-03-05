6306709372333bae6906fca8d8acd0ae4bc2d059
vinicius.mamore@gmail.com <vinicius.mamore@gmail.com>
2019-03-05 10:34:03 -0400

    Criação de componentes para cada parte do index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,17 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { MenuSuperiorComponent } from './shared/menu-superior/menu-superior.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainPrincipalComponent } from './shared/main-principal/main-principal.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    MenuSuperiorComponent,
+    FooterComponent,
+    MainPrincipalComponent
+  ],
   imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/menu-superior/menu-superior.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-superior',
+  templateUrl: './menu-superior.component.html',
+  styleUrls: []
+})
+export class MenuSuperiorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
